aa9c7b20c7e60e134caee8655413f89cdc51b569
Steven Oh <stevenoh0908@gmail.com>
2018-11-23 19:51:00 +0900

    v0.0.1

---
+++ flightcombat.py
@@ -0,0 +1,136 @@
+#flightcombat.py
+__author__ = 'stdio.chem@gmail.com'
+
+import pygame, random
+import color
+
+pad_width = 480
+pad_height = 720
+caption = "Flight Combat"
+
+def drawObject(obj, x, y):
+    global gamepad
+    gamepad.blit(obj,(x,y))
+    pass
+
+def initGame():
+    global gamepad, clock, background1, background2
+    global player, enemy, meteor, bullet
+
+    pygame.init()
+
+    gamepad = pygame.display.set_mode((pad_width,pad_height))
+    pygame.display.set_caption(caption)
+
+    background1 = pygame.image.load('image/background.png')
+    background2 = background1.copy()
+    player = pygame.image.load('image/player.png')
+    bullet = pygame.image.load("image/bullet.png")
+    
+    clock = pygame.time.Clock()
+    runGame()
+    pass
+
+def runGame():
+    global gamepad, clock, background1, background2
+    global player, enemy, meteor, bullet
+
+    bullet_xy = []
+    
+    speed = 5
+    
+    background1_y = 0
+    background2_y = -pad_height
+    
+    player_x = 200
+    player_y = 620
+    player_x_change = 0
+    player_y_change = 0
+
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player_y_change = -5
+                    pass
+                elif event.key == pygame.K_DOWN:
+                    player_y_change  = 5
+                    pass
+                elif event.key == pygame.K_RIGHT:
+                    player_x_change = 5
+                    pass
+                elif event.key == pygame.K_LEFT:
+                    player_x_change = -5
+                    pass
+                elif event.key == pygame.K_SPACE:
+                    bullet_xy.append([player_x + 11, player_y])
+                    pass
+                pass
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN or pygame.K_UP:
+                    player_y_change = 0
+                if event.key == pygame.K_RIGHT or pygame.K_LEFT:
+                    player_x_change = 0
+                pass
+            pass
+
+        background1_y += speed
+        background2_y += speed
+
+        if background1_y > pad_height:
+            background1_y = -pad_height
+            pass
+        if background2_y > pad_height:
+            background2_y = -pad_height
+            pass
+            
+        player_x += player_x_change
+        player_y += player_y_change
+
+        if player_x > pad_width - 80:
+            player_x = pad_width - 80
+            pass
+        elif player_x < 0:
+            player_x = 0
+            pass
+        if player_y > pad_height - 80:
+            player_y = pad_height - 80
+            pass
+        elif player_y < 0:
+            player_y = 0
+            pass
+
+        if not len(bullet_xy) == 0:
+            for i, bxy in enumerate(bullet_xy):
+                bxy[1] -= 15
+                bullet_xy[i][1] = bxy[1]
+                if bxy[1] <= -80:
+                    bullet_xy.remove(bxy)
+                    pass
+                pass
+            pass
+        
+        gamepad.fill((6,17,39))
+
+        drawObject(background1,0,background1_y)
+        drawObject(background2,0,background2_y)
+
+        if not len(bullet_xy) == 0:
+            for bx, by in bullet_xy:
+                drawObject(bullet,bx,by)
+                pass
+            pass
+        
+        drawObject(player, player_x, player_y)
+        
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+    quit()
+    pass
+
+initGame()
